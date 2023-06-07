fee52ae39f481cfa7c9cd1b483f8d2face89e711
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-06-06 19:10:02 -0500

    upload exercises

---
+++ area_circunferencia_circulo.py
@@ -0,0 +1,11 @@
+from math import pi
+r = float(input('Escriba el valor del radio: '))
+
+area = pi * r ** 2
+
+print('El area del circulo es igual a {:.2f}'.format(area))
+
+circunferencia = 2 * pi * r
+
+print('La circunferencia del circulo es igual a  {:.2f}'.format(
+    circunferencia))
